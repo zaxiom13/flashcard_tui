83d825b35568637288589db60f6c7f490114c354
zaxiom13 <58856427+zaxiom13@users.noreply.github.com>
2023-03-15 19:44:30 +0000

    basic cmd line version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Flashcard {
+    question: String,
+    answer: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let flashcards = vec![
+        Flashcard {
+            question: String::from("What is the capital of France?"),
+            answer: String::from("Paris"),
+        },
+        Flashcard {
+            question: String::from("What is 2 + 2?"),
+            answer: String::from("4"),
+        },
+    ];
+
+    for flashcard in flashcards {
+        println!("{}", flashcard.question);
+        let mut user_answer = String::new();
+        std::io::stdin()
+            .read_line(&mut user_answer)
+            .expect("Failed to read user input");
+
+        if user_answer.trim() == flashcard.answer {
+            println!("Correct!");
+        } else {
+            println!("Incorrect. The correct answer is: {}", flashcard.answer);
+        }
+    }
 }
